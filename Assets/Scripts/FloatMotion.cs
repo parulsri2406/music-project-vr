e2b520183271658f4870e793b7ad09aaf95e06e8
Parul Srivastava <parulsri240605@gmail.com>
2026-04-02 04:16:48 +0530

    rotation issue fixed

--- Assets/Scripts/FloatMotion.cs
+++ Assets/Scripts/FloatMotion.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class FloatMotion : MonoBehaviour
 {
@@ -7,25 +8,60 @@ public class FloatMotion : MonoBehaviour
 
     [Header("Rotation")]
     public float rotationSpeed = 20f;
-    public bool shouldRotate = false; // controlled externally
+    public bool shouldRotate = false;
 
     private Vector3 startPos;
+    private bool isGrabbed = false;
+
+    private XRGrabInteractable grab;
+
+    void Awake()
+    {
+        grab = GetComponent<XRGrabInteractable>();
+    }
 
     void Start()
     {
         startPos = transform.localPosition;
     }
 
+    void OnEnable()
+    {
+        grab.selectEntered.AddListener(OnGrab);
+        grab.selectExited.AddListener(OnRelease);
+    }
+
+    void OnDisable()
+    {
+        grab.selectEntered.RemoveListener(OnGrab);
+        grab.selectExited.RemoveListener(OnRelease);
+    }
+
+    void OnGrab(SelectEnterEventArgs args)
+    {
+        isGrabbed = true;
+    }
+
+    void OnRelease(SelectExitEventArgs args)
+    {
+        isGrabbed = false;
+
+        // Reset base position when released (so float resumes cleanly)
+        startPos = transform.localPosition;
+    }
+
     void Update()
     {
-        // float forever
-        float offset = Mathf.Sin(Time.time * speed) * amplitude;
-        transform.localPosition = startPos + new Vector3(0, offset, 0);
-
-        // Rotation 4 enable
-        if (shouldRotate)
+        // ONLY float when NOT grabbed
+        if (!isGrabbed)
         {
-            transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+            float offset = Mathf.Sin(Time.time * speed) * amplitude;
+            transform.localPosition = startPos + new Vector3(0, offset, 0);
+
+            if (shouldRotate)
+            {
+                transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+            }
         }
     }
 }
